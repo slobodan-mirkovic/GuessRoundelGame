aa4c04a086989486e6a7929126bc9bc051b7d5ac
slobodan-mirkovic <mirkovic.sloba@gmail.com>
2016-01-23 17:52:54 +0100

    initial

---
+++ js/game.js
@@ -0,0 +1,48 @@
+
+function getRandomNumber(max) {
+	return Math.floor(Math.random() * max) ;
+} 
+
+
+function getRandomNumbers(n,max) {
+	
+	var list = new Array();
+	
+	list.push(getRandomNumber(max));
+	
+	while(list.length < n){
+		var random = getRandomNumber(max);
+		if(list.indexOf(random) == -1){
+			list.push(random);
+		}
+	}	
+	return list;
+} 
+
+  var list = getRandomNumbers(6,10);
+  
+  var roundel = getRandomNumber(6);
+  
+  list.forEach(function(element){
+	var galery = document.getElementById("flexcontainer");
+
+    var listItem = document.createElement('div');
+	listItem.className = "item";
+    var img = document.createElement('img');
+    img.src = "img/" + roundels[element].roundel ;
+    listItem.appendChild(img);
+    galery.appendChild(listItem);
+	
+	document.getElementById("roundel").innerText = roundels[roundel].airforce;
+	  
+  });
+  
+  var roundelsImg = document.querySelectorAll("#flexcontainer .item");
+  for(var i = 0; i < roundelsImg.length ; i++){
+	   roundelsImg[i].addEventListener('click', function () {alert('sasa');});
+  };
+
+	
+	
+
+
---
+++ js/roundels.js
@@ -0,0 +1,14 @@
+
+
+var roundels = [
+	{ id: 1, airforce: "Serbian Air Force", roundel: "Roundel_of_the_Serbian_Air_Force_2006.svg"},
+	{ id: 2, airforce: "Montenegrin Air Force", roundel: "Roundel_of_the_Montenegran_Air_Force.svg"},
+	{ id: 3, airforce: "Russian Air Force", roundel: "URSS-Russian_aviation_red_star.svg"},
+	{ id: 4, airforce: "French Air Force", roundel: "French-roundel.svg"},
+	{ id: 5, airforce: "United States Air Force", roundel: "Roundel_of_the_USAF.svg"},
+	{ id: 6, airforce: "Royal Air Force", roundel: "RAF_roundel.svg"},
+	{ id: 7, airforce: "Royal Australian Air Force", roundel: "RAAF_Roundel.svg"},
+	{ id: 8, airforce: "Royal New Zealand Air Force", roundel: "Rnzaf_roundel.svg"},
+	{ id: 9, airforce: "Spanish Air Force", roundel: "Roundel_of_the_Spanish_Air_Force.svg"},
+	{ id: 10, airforce: "Italian Air Force", roundel: "LV_Italian_Air_Force_roundel.svg"}
+];
