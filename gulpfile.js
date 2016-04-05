3713b0059f5244855d2709df27696273f63a83a1
Boban <mirkovic.sloba@gmail.com>
2016-04-05 21:51:39 +0200

    simple-gulp-tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,11 @@
 'use strict';
 
 var gulp = require('gulp');
+
+gulp.task("hello", function(){
+    console.log("Hello!");
+});
+
+gulp.task('default', ["hello"], function (){
+    console.log("This is default task.");
+});
