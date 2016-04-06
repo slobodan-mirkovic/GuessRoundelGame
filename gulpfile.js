f389c9e98b45fda1e74e9cdc9a435bdc4c93d64c
Boban <mirkovic.sloba@gmail.com>
2016-04-06 20:29:12 +0200

    clean-min

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,40 @@
 'use strict';
 
 var gulp = require('gulp'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rimraf = require("rimraf"),
+    cssmin = require("gulp-cssmin");
 
-gulp.task("concatScripts", function(){
-    gulp.src([
+
+gulp.task("clean:js", function (cb) {
+    rimraf('js/app.min.js', cb);
+});
+
+gulp.task("clean:css", function (cb) {
+    rimraf('css/app.min.css', cb);
+});
+
+gulp.task("clean", ["clean:js", "clean:css"]);
+
+gulp.task("min:js", function () {
+    return gulp.src([
         'js/roundels.js',
         'js/game.js',
         'js/main.js'])
-        .pipe(concat('app.js'))
-        .pipe(gulp.dest('js'));
+        .pipe(concat("app.min.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest("js"));
 });
 
-gulp.task('default', ["hello"], function (){
-    console.log("This is default task.");
+gulp.task("min:css", function () {
+    return gulp.src([
+        'css/normalze.css',
+        'css/main.css'])
+        .pipe(concat("app.min.css"))
+        .pipe(cssmin())
+        .pipe(gulp.dest("css"));
 });
+
+gulp.task("min", ["min:js", "min:css"]);
+
--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
 
-'use strict'
+'use strict';
 
 function getRandomNumber(max) {
 	return Math.floor(Math.random() * max) ;
